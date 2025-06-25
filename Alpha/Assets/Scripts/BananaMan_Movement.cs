b7a356a3911359057419dd2bb97045a442f04064
CianDonnelly21 <b00160418@mytudublin.ie>
2025-06-25 01:05:49 +0100

    Colliders/Rigidbodies/NewCharacter

--- Alpha/Assets/Scripts/BananaMan_Movement.cs
+++ Alpha/Assets/Scripts/BananaMan_Movement.cs
@@ -3,14 +3,17 @@ using UnityEngine;
 public class BananMan_Movement : MonoBehaviour {
 
 public float speed = 0.1f;
+public GameObject CrouchMan;
+public GameObject BananaMan;
 
 void Start() 
 {
-
+    CrouchMan.gameObject.SetActive(false);
+    BananaMan.gameObject.SetActive(true);
 }
 
 void Update()
-{
+    {
         if (Input.GetKey(KeyCode.D))
         {
             transform.Translate(Vector3.forward * speed);
@@ -20,5 +23,31 @@ void Update()
         {
             transform.Translate(Vector3.back * speed);
         }
-}
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.Translate(Vector3.up );
+        }
+        //Double Jump
+        if (Input.GetKey(KeyCode.R))
+        {
+            transform.Translate(Vector3.up * 7);
+        }
+
+        //Crouch 
+        if (Input.GetKey(KeyCode.S))
+        {
+            BananaMan.transform.position = transform.position;
+
+            CrouchMan.gameObject.SetActive(true);
+            BananaMan.gameObject.SetActive(false);
+        }
+        else 
+        {
+            CrouchMan.transform.position = transform.position;
+
+            CrouchMan.gameObject.SetActive(false);
+            BananaMan.gameObject.SetActive(true);
+        }
+    }
 }
